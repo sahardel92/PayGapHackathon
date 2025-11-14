b51c5e13eaa1745127cdea0332ba35c95597d372
Victoria P <victoria@test.be>
2025-11-14 13:47:36 +0100

    final workshop css/html/js

--- Frontend/workshop.js
+++ Frontend/workshop.js
@@ -1,8 +1,8 @@
-  document.getElementById('downloadBtn').addEventListener('click', function() {
+document.getElementById('downloadBtn').addEventListener('click', function() {
     const link = document.createElement('a');
     link.href = 'images/paygap.pdf'; // path to your PDF
-    link.download = 'myfile.pdf';   // filename for download
+    link.target = '_blank';          // OPEN IN NEW TAB
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-  });
+});
